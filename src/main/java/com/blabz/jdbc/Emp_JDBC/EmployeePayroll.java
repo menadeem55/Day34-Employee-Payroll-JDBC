c4cccf69f0079bf5082ec5fa312f594ba5ec418a
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-02-20 15:47:23 +0530

    Added: update the database by using Prepared Statement

--- src/main/java/com/blabz/jdbc/Emp_JDBC/EmployeePayroll.java
+++ src/main/java/com/blabz/jdbc/Emp_JDBC/EmployeePayroll.java
@@ -4,80 +4,29 @@ import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Enumeration;
 
 public class EmployeePayroll {
-	static Connection con = null;
-
-	public static void main(String[] args) throws EmployeeCustomException, SQLException {
-		con = connected();
-		reteriveData(con);
-		updateData(con);
-
-	}
-
-	public static Connection connected() throws EmployeeCustomException {
-		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false"; // declare JdbcURL
-		String UserName = "root";
-		String Password = "rootroot";
-		Connection connection = null;
+	public static void preparedStatement(String name, double salary) {
 
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver"); // inbuilt method for Class.forName for loading driver
-			System.out.println("Driver loaded");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root",
+					"rootroot");
+			String s = "update employee_payroll set salary = ? where name=?";
+			PreparedStatement ps = conn.prepareStatement(s);
+			ps.setDouble(1, salary);
+			ps.setString(2, name);
+			ps.executeUpdate();
+			boolean result = true;
 
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace(); // for tracing the exception
-
-		}
-		listDrivers(); // static method calling
-		try {
-			System.out.println("Connecting to Database...:" + jdbcURL); // for loading the drive for connect
-			connection = DriverManager.getConnection(jdbcURL, UserName, Password);
-			System.out.println("coneection successfull" + connection);
-
-		} catch (Exception e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
-
 		}
-		return connection;
 
 	}
 
-	public static void reteriveData(Connection connection) throws EmployeeCustomException, SQLException {
-		PreparedStatement ps = connection.prepareStatement("Select * from employee_payroll");
-		ResultSet result = ps.executeQuery();
-		while (result.next()) {
-			System.out.print(result.getInt(1));
-			System.out.print(" | ");
-			System.out.print(result.getString(2));
-			System.out.print(" | ");
-			System.out.print(result.getString(3));
-			System.out.print(" | ");
-			System.out.print(result.getDouble(4));
-			System.out.print(" | ");
-			System.out.println();
-		}
-	}
-
-	public static void updateData(Connection connection) throws EmployeeCustomException, SQLException {
-		PreparedStatement ps = connection.prepareStatement("update employee_payroll set salary = ? where id = ?;");
-		ps.setDouble(1, 3100000.78);
-		ps.setInt(2, 1);
-
-		ps.executeUpdate();
-		System.out.println("Update Successfully");
-	}
-
-	public static void listDrivers() {
-		Enumeration<Driver> driverList = DriverManager.getDrivers();
-		while (driverList.hasMoreElements()) { // static method for iteration.
-			Driver driverClass = (Driver) driverList.nextElement();
-			System.out.println(" " + driverClass.getClass().getName());
-
-		}
+	public static void main(String[] args) throws SQLException {
+		preparedStatement("Bill", 450000.55);
 
 	}
 }
