9c7ca7925ee4405c5a7d8ef30a4224a4629715b5
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-02-20 15:38:10 +0530

    Connecting to JDBC

--- src/main/java/com/blabz/jdbc/Emp_JDBC/EmployeePayroll.java
+++ src/main/java/com/blabz/jdbc/Emp_JDBC/EmployeePayroll.java
@@ -1,8 +1,46 @@
 package com.blabz.jdbc.Emp_JDBC;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
 
 public class EmployeePayroll {
 	public static void main(String[] args) {
-		System.out.println("welcome to Employee payroll JDBC");
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false"; // declare JdbcURL
+		String UserName = "root";
+		String Password = "rootroot";
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); // inbuilt method for Class.forName for loading driver
+			System.out.println("Driver loaded");
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace(); // for tracing the exception
+
+		}
+		listDrivers();
+		try {
+			System.out.println("Connecting to Database...: " + jdbcURL); // for loading the drive for connect
+			Connection connection = DriverManager.getConnection(jdbcURL, UserName, Password); // creating object for
+																								// connection
+			System.out.println("coneection successfull");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
 	}
+
+	public static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println(" " + driverClass.getClass().getName());
+
+		}
+
+	}
+
 }
