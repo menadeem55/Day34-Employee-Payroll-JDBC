7336d35fe86f50246660bb803e57b4b25fd1aac2
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-02-20 15:43:06 +0530

    Added: Updating salary in existing table

--- src/main/java/com/blabz/jdbc/Emp_JDBC/EmployeePayroll.java
+++ src/main/java/com/blabz/jdbc/Emp_JDBC/EmployeePayroll.java
@@ -14,6 +14,7 @@ public class EmployeePayroll {
 	public static void main(String[] args) throws EmployeeCustomException, SQLException {
 		con = connected();
 		reteriveData(con);
+		updateData(con);
 
 	}
 
@@ -31,7 +32,7 @@ public class EmployeePayroll {
 			e.printStackTrace(); // for tracing the exception
 
 		}
-		listDrivers();		//	static method calling
+		listDrivers(); // static method calling
 		try {
 			System.out.println("Connecting to Database...:" + jdbcURL); // for loading the drive for connect
 			connection = DriverManager.getConnection(jdbcURL, UserName, Password);
@@ -55,13 +56,24 @@ public class EmployeePayroll {
 			System.out.print(" | ");
 			System.out.print(result.getString(3));
 			System.out.print(" | ");
+			System.out.print(result.getDouble(4));
+			System.out.print(" | ");
 			System.out.println();
 		}
 	}
 
+	public static void updateData(Connection connection) throws EmployeeCustomException, SQLException {
+		PreparedStatement ps = connection.prepareStatement("update employee_payroll set salary = ? where id = ?;");
+		ps.setDouble(1, 3100000.78);
+		ps.setInt(2, 1);
+
+		ps.executeUpdate();
+		System.out.println("Update Successfully");
+	}
+
 	public static void listDrivers() {
 		Enumeration<Driver> driverList = DriverManager.getDrivers();
-		while (driverList.hasMoreElements()) {				//static method for itteration.
+		while (driverList.hasMoreElements()) { // static method for iteration.
 			Driver driverClass = (Driver) driverList.nextElement();
 			System.out.println(" " + driverClass.getClass().getName());
 
