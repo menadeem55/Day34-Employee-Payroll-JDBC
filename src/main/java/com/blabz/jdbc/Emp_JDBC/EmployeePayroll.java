6afcd9e10251bfee95810c8e7a6d49e3b76b5f53
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-02-21 00:12:42 +0530

    Added: Reterive all data prepared Statement

--- src/main/java/com/blabz/jdbc/Emp_JDBC/EmployeePayroll.java
+++ src/main/java/com/blabz/jdbc/Emp_JDBC/EmployeePayroll.java
@@ -4,30 +4,37 @@ import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class EmployeePayroll {
-	public static boolean CreatedConnection(String s) {
-		return true;
-
-	}
-
-	public static boolean preparedStatement(String name, double salary) {
-		Connection conn;
+	public static void CreatedConnection() {
 
 		try {
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root", "rootroot");
-			String s = "update employee_payroll set salary = ? where name=?";
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root",
+					"rootroot");
+			String s = "select * from employee_payroll";
 			PreparedStatement ps = conn.prepareStatement(s);
-			ps.setDouble(1, salary);
-			ps.setString(2, name);
-			ps.executeUpdate();
-			boolean result = true;
+			ResultSet result = ps.executeQuery();
+
+			while (result.next()) {
+				int id = result.getInt(1);
+				String name = result.getString(2);
+				String gender = result.getString(3);
+				Double salary = result.getDouble(4);
+				String date = result.getString(5);
+				System.out.println("id:- " + id + " Name:- " + name + " Gender:- " + " | " + " Salary:- " + salary
+						+ " Date:- " + date);
+			}
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return true;
+
+	}
+
+	public static void main(String[] args) throws SQLException {
+		CreatedConnection();
 
 	}
 }
